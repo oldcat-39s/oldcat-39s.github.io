cd795bd7e3a72d667848b6a200c4cf244399f991
oldcat <oldcat@39s.moe>
2026-09-02 11:53:49 +0800

    update

---
+++ js/status.js
@@ -0,0 +1,10 @@
+function updateStatus() {
+    document.getElementById('status-text').innerHTML =
+    `<a href="/status.html">
+    2026/09/02 11:32 am <br>
+    天气变冷了...要是一直这样就好了。
+    </a>
+    `
+}
+
+updateStatus();
